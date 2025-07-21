eda8dcb7ec8f5354142d58247162dbdc69f6a06b
GeladinhoPlayer <geladinhosnf@gmail.com>
2025-07-21 03:34:48 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,19 @@
 -- loader.lua
+
+-- Créditos
+game.StarterGui:SetCore("SendNotification", {
+    Title = "🧠 Executor por GeladinhoPlayer";
+    Text = "Criador do script: GeladinhoPlayer";
+    Duration = 5;
+})
+
+print("✅ Script Executor por: GeladinhoPlayer")
+print("⏳ Carregando o menu...")
+
+-- Aguarda 3 segundos antes de carregar o menu
+task.wait(3)
+
+-- Carrega o menu/script principal
 local scriptUrl = "https://raw.githubusercontent.com/GeladinhoPlayer/Loader2/main/script99.lua"
 local loaded, err = loadstring(game:HttpGet(scriptUrl, true))
 
