83ea3490ece9030f9fb1495a7d8713fd9eeb3c7d
GeladinhoPlayer <geladinhosnf@gmail.com>
2025-07-21 03:32:54 -0300

    Create script99.lua

---
+++ script99.lua
@@ -0,0 +1,8 @@
+-- script99.lua
+game.StarterGui:SetCore("SendNotification", {
+    Title = "99 Dias na Floresta 🌲";
+    Text = "Script executado com sucesso!";
+    Duration = 5;
+})
+
+print("✅ Script '99 Dias na Floresta' iniciado!")
