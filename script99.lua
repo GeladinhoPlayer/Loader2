d1226acd3404909a795a2734a7fe4c1ed53df59f
GeladinhoPlayer <geladinhosnf@gmail.com>
2025-07-22 03:49:34 -0300

    Update script99.lua

--- script99.lua
+++ script99.lua
@@ -1,94 +1,87 @@
--- Carrega a Orion Library
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/jensonhirst/Orion/main/Source.lua"))()
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+OrionLib:MakeNotification({
+	Name = "Title!",
+	Content = "Notification content... what will it say??",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+Tab:AddToggle({
+	Name = "This is a toggle!",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+Tab:AddColorpicker({
+	Name = "Colorpicker",
+	Default = Color3.fromRGB(255, 0, 0),
+	Callback = function(Value)
+		print(Value)
+	end	  
+})
+-- ColorPicker:Set(Color3.fromRGB(255,255,255))
+Tab:AddSlider({
+	Name = "Slider",
+	Min = 0,
+	Max = 20,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "bananas",
+	Callback = function(Value)
+		print(Value)
+	end    
+})
 
--- Função para criar o menu principal
-local function createMainMenu()
-    local Window = OrionLib:MakeWindow({
-        Name = "LightScripts - Menu Principal",
-        HidePremium = false,
-        SaveConfig = true,
-        ConfigFolder = "LightScripts"
-    })
+ --Slider:Set(2)
+ Tab:AddLabel("Label")
+--CoolLabel:Set("Label New!")
+Tab:AddParagraph("Paragraph","Paragraph Content")
+-- CoolParagraph:Set("Paragraph New!")
 
-    -- Aba de Voo
-    local FlyTab = Window:MakeTab({
-        Name = "Voo",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
+Tab:AddTextbox({
+	Name = "Textbox",
+	Default = "default box input",
+	TextDisappear = true,
+	Callback = function(Value)
+		print(Value)
+	end	  
+})
 
-    -- Variáveis para o sistema de voo
-    local flyEnabled = false
-    local flySpeed = 50
-    local bodyGyro, bodyVelocity
 
-    -- Função para ativar o voo
-    local function setupFly()
-        local char = game.Players.LocalPlayer.Character
-        if not char or not char:FindFirstChild("HumanoidRootPart") then return end
-        bodyGyro = Instance.new("BodyGyro", char.HumanoidRootPart)
-        bodyGyro.P = 9e4
-        bodyGyro.MaxTorque = Vector3.new(1e9, 1e9, 1e9)
-        bodyVelocity = Instance.new("BodyVelocity", char.HumanoidRootPart)
-        bodyVelocity.MaxForce = Vector3.new(1e9, 1e9, 1e9)
-        game:GetService("RunService"):BindToRenderStep("Fly", Enum.RenderPriority.Character.Value, function()
-            local cam = workspace.CurrentCamera.CFrame
-            local move = Vector3.new()
-            if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.W) then move += cam.LookVector end
-            if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.S) then move -= cam.LookVector end
-            if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.A) then move -= cam.RightVector end
-            if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.D) then move += cam.RightVector end
-            if move.Magnitude > 0 then
-                bodyVelocity.Velocity = move.Unit * flySpeed
-                bodyGyro.CFrame = CFrame.new(char.HumanoidRootPart.Position, char.HumanoidRootPart.Position + cam.LookVector)
-            else
-                bodyVelocity.Velocity = Vector3.new(0, 0, 0)
-            end
-        end)
-    end
-
-    -- Função para desativar o voo
-    local function disableFly()
-        game:GetService("RunService"):UnbindFromRenderStep("Fly")
-        if bodyGyro then bodyGyro:Destroy() end
-        if bodyVelocity then bodyVelocity:Destroy() end
-    end
-
-    -- Toggle para ativar/desativar o voo
-    FlyTab:AddToggle({
-        Name = "Ativar Voo",
-        Default = false,
-        Callback = function(value)
-            flyEnabled = value
-            if flyEnabled then setupFly() else disableFly() end
-        end
-    })
-
-    -- Slider para ajustar a velocidade do voo
-    FlyTab:AddSlider({
-        Name = "Velocidade do Voo",
-        Min = 10,
-        Max = 200,
-        Default = 50,
-        Increment = 10,
-        Suffix = " studs/s",
-        Callback = function(v)
-            flySpeed = v
-        end
-    })
-
-    -- Fechar o menu
-    FlyTab:AddButton({
-        Name = "Fechar Menu",
-        Callback = function()
-            if flyEnabled then disableFly() end
-            OrionLib:Destroy()
-        end
-    })
-end
-
--- Chama a função para criar o menu principal
-createMainMenu()
-
--- Inicializa a Orion Library
+Tab:AddBind({
+	Name = "Bind",
+	Default = Enum.KeyCode.E,
+	Hold = false,
+	Callback = function()
+		print("press")
+	end    
+})
+-- Bind:Set(Enum.KeyCode.E)
+Tab:AddDropdown({
+	Name = "Dropdown",
+	Default = "1",
+	Options = {"1", "2"},
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+-- Dropdown:Refresh(List<table>,true)
+--Dropdown:Set("dropdown option")
 OrionLib:Init()
+-- destroying the interface: OrionLib:Destroy()
